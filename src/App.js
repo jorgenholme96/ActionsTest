0c716eacf9694fc7a2cfe6ea560c10089b92d7dc
jorgenholme96 <jorgen.holme@bouvet.no>
2021-06-28 23:23:13 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const hey = "hey";
+  console.log(hey)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
